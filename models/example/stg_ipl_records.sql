5f89f9ab4168f9c74e0dbb1e30183f52a7d9b23d
Nishant Bhaskar <nishantbhaskar827@gmail.com>
2024-04-13 19:27:14 +0000

    ball_count

--- models/example/stg_ipl_records.sql
+++ models/example/stg_ipl_records.sql
@@ -1,1 +1,6 @@
-insert into ipl_records.ball_count VALUES 1;
+select * except(ball_count), 
+    case 
+        when ball_count is null then 1
+    end as ball_count
+from neat-vent-383007.dbt_nbhaskar.ipl_records
+

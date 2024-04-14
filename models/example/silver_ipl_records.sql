b345300c58561ecf42b751a5c5bc579a13eb8cf9
Nishant Bhaskar <nishantbhaskar827@gmail.com>
2024-04-14 06:44:57 +0000

    select required data

---
+++ models/example/silver_ipl_records.sql
@@ -0,0 +1,3 @@
+select venue,innings,batting_team,bowling_team,striker,bowler,runs_off_bat,wicket_type,player_dismissed,ball_count 
+from neat-vent-383007.dbt_nbhaskar.stg_ipl_records
+where wicket_type in ('caught','bowled','lbw','caught and bowled','stumped','hit wicket') or wicket_type is null

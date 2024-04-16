f72b81dfa7d0b1f2e6b12f47aea5d15d33ce81ba
Nishant Bhaskar <nishantbhaskar827@gmail.com>
2024-04-16 07:04:31 +0000

    macro

--- macros/venue.sql
+++ macros/venue.sql
@@ -1,7 +1,7 @@
 {%macro venue_filter_data(venue)%} (
-select striker,bowler,sum(runs_off_bat) as run_scored ,count(wicket_type) as number_of_dismissal,sum(ball_count) as number_of_ball_face from neat-vent-383007.dbt_nbhaskar.silver_ipl_records
+select venue,striker,bowler,sum(runs_off_bat) as run_scored ,count(wicket_type) as number_of_dismissal,sum(ball_count) as number_of_ball_face from neat-vent-383007.dbt_nbhaskar.silver_ipl_records
 where venue = venue
-GROUP BY striker,bowler
+GROUP BY venue,striker,bowler
 )
 {% endmacro %}
 

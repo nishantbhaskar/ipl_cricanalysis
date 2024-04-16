3e9473d0b4edd3b1075e15e7ea0f1bdfab5f7c5f
Nishant Bhaskar <nishantbhaskar827@gmail.com>
2024-04-16 09:40:39 +0000

    macro

--- macros/best_bastman_vs_spec_bowler.sql
+++ macros/best_bastman_vs_spec_bowler.sql
@@ -1,4 +1,4 @@
-{{% macro best_bastman_vs_spec_bowler(bowler)%}(
+{% macro best_bastman_vs_spec_bowler(bowler)%}(
 
  SELECT * FROM `neat-vent-383007.dbt_nbhaskar.gold_ipl_records` where bowler='{{bowler}}'  and number_of_ball_face>5 order by average desc
 )

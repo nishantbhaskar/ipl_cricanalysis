ad6073e222dc0870b9fa9f9f6fa9e4829b5026af
Nishant Bhaskar <nishantbhaskar827@gmail.com>
2024-05-16 12:53:13 +0000

    srh vs gt

--- models/cricanalysis/stg_ipl_records.sql
+++ models/cricanalysis/stg_ipl_records.sql
@@ -1,6 +1,4 @@
-select * except(ball_count), 
-    case 
-        when ball_count is null then 1
-    end as ball_count
+{{ config(materialized='view') }}
+select *,1 as ball_count 
 from neat-vent-383007.dbt_nbhaskar.ipl_records
 

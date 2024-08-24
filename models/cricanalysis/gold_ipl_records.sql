4674b47b7f2dc9f38e278befbd9d9bd8da93e0b0
Nishant Bhaskar <nishantbhaskar827@gmail.com>
2024-08-24 10:35:16 +0000

    dbt

--- models/cricanalysis/gold_ipl_records.sql
+++ models/cricanalysis/gold_ipl_records.sql
@@ -1,2 +1,2 @@
 {{ config(materialized='view') }}
-select * , (SAFE_DIVIDE(run_scored,number_of_ball_face))*100 as strikerate,SAFE_DIVIDE(run_scored,number_of_dismissal) as average from neat-vent-383007.dbt_nbhaskar.venue_filter_ipl
+select * , (SAFE_DIVIDE(run_scored,number_of_ball_face))*100 as strikerate,SAFE_DIVIDE(run_scored,number_of_dismissal) as average from neat-vent-383007.dbt_nbhaskar.without_venue_filter

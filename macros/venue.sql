9ecf3b5555baadd50a71c27d9568c63d8a0e7588
Nishant Bhaskar <nishantbhaskar827@gmail.com>
2024-08-24 10:30:20 +0000

    dbt

--- macros/venue.sql
+++ macros/venue.sql
@@ -1,6 +1,5 @@
 {%macro venue_filter_data(venue)%} (
 select venue,striker,bowler,sum(runs_off_bat) as run_scored ,count(wicket_type) as number_of_dismissal,sum(ball_count) as number_of_ball_face from neat-vent-383007.dbt_nbhaskar.silver_ipl_records
-where REGEXP_CONTAINS(LOWER(venue), r'{{venue}}')
 GROUP BY venue,striker,bowler
 )
 {% endmacro %}
